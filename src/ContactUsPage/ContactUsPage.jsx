cd32d06ac3c7b9ba751a14419b0f805bb0d8df85
argun.muradov@ug.bilkent.edu.tr <argun.muradov@ug.bilkent.edu.tr>
2021-12-19 18:37:04 +0300

    Initial code commit

---
+++ src/ContactUsPage/ContactUsPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { userService, authenticationService } from '@/_services';
+
+class ContactUsPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+}
+
+    render() {
+        return (
+            <div>
+                <h1>ContactUsPage </h1>
+            </div>
+        );
+    }
+}
+
+export { ContactUsPage };
---
+++ src/PublicEventsList/PublicEventsList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { userService, authenticationService } from '@/_services';
+
+class PublicEventsList extends React.Component {
+    constructor(props) {
+        super(props);
+
+}
+
+    render() {
+        console.log("saifjhasiuf")
+        return (
+            <div>
+                <h1>Public Events</h1>
+            </div>
+        );
+    }
+}
+
+export { PublicEventsList };
