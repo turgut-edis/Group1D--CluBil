47de2a814bc5ae391b9a82c11c90c6f764237e52
Taha-Batur-Senli <63501539+Taha-Batur-Senli@users.noreply.github.com>
2021-12-25 15:29:44 +0300

    Added admin profile page

--- src/UserProfilePage.js
+++ src/UserProfilePage.js
@@ -237,6 +237,94 @@ function UserProfilePage() {
           </>
     );
   }
+  else if(role === "admin")
+  {
+    return (
+        <>
+
+      <nav class="navbar navbar-expand-sm navbar-dark navbar-custom">
+
+      <div class="container-fluid">
+        <button
+        class="navbar-toggler"
+        type="button"
+        data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+
+      <i class="fas fa-bars"></i>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img
+            src="https://w3.bilkent.edu.tr/logo/ing-amblem.png"
+            height="35"
+            alt="MDB Logo"
+            loading="lazy"
+          />
+        </a>
+      
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/first">Event List</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/calendar">Calendar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/clubs">Clubs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/finance">Finance</a>
+          </li>
+        </ul>
+      </div>
+      
+      <div class="d-flex align-items-center">
+      <div class="navbar-text username-css">{name}</div>
+          <a href="/userprofilepage">
+            <img
+            src="https://png.pngitem.com/pimgs/s/238-2388363_comment-from-static-noise-profile-picture-vector-hd.png"
+            class="rounded-circle"
+            height="35"
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+            />
+          </a>
+          <button type="button" class="btn btn-primary logout-button" onClick={logout}>Logout</button>
+      </div>
+      
+      </div>
+      </nav>
+      <div class="d-flex align-items-center profilepic">
+            <img
+            src="https://png.pngitem.com/pimgs/s/238-2388363_comment-from-static-noise-profile-picture-vector-hd.png"
+            class="rounded-circle"
+            max width="200"
+            max height="200"
+            alt="Black and White Portrait of a Man"
+            loading="lazy"
+            />
+            
+            <div class="profileinfo-css"><strong>Full Name:</strong> {name} <br />
+                                      <strong>Role:</strong> {role} <br />
+                                      </div>
+      </div>
+
+      <div class="nametext"> <strong>{name}</strong> </div>
+        
+  <br />
+      <button type="button" class="btn btn-primary changepassw nametext" onClick={logout}>Change Password</button>
+
+  <br />
+      <button type="button" class="btn btn-primary editprofile nametext" onClick={logout}>Edit Profile</button>
+          </>
+    );
+  }
   else
   {
     return (<div class="nametext"> <strong>31</strong> </div>);
