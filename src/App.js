b50ac0dd198a766f771dc98ed29b5b965e713799
Doğukan Kurnaz <dogukanertunga@gmail.com>
2021-12-26 03:26:39 +0300

    publicEvents page refactored with the dynamically filled elements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import PublicEvent from "./components/PublicEvent";
+import Manage from "./managers/ManagerFacade";
+
 
 function App() {
   const history = useNavigate();
+  const [data, setData] = useState();
+
+  const fetchAllEventData = async () => {
+    var b = await Manage("event").getAllEvents()
+    setData(b)
+  }
+
+  useEffect(async () => {
+    await fetchAllEventData()
+  }, []);
+
+  if (data == null) {
+    return <div>loading...</div>
+  }
+  if (data != null)
+    console.log("data")
 
   const routeChange = (route) => {
     history(route);
-  };
-  //const commonProps = {name: 'ORFest 2019',location: 'Bilkent Sports International', time: "21:00"};
-  return (
+  }
+
+  return(
+    <>
     <div>
       <nav className="button-holder">
         <Button variant="primary" className="btn btn-primary login-page-button" onClick={() => routeChange("login")}>
@@ -20,33 +40,32 @@ function App() {
         </Button>
       </nav>
       <div className="display-5 public-events-title">Open Access Events</div>
+      </div>
+
+
+      {data.map((elementInArray, index) => {
+        console.log(elementInArray, index)
+        return (
       <div className="public-events">
         <PublicEvent
-          name="ORFest 2019 🎊"
-          location="Bilkent"
-          time="21:00"
-          description="Littest party of 2019"
-          eventKey="0"
-        />
-        <PublicEvent
-          name="Pizza Party🍕"
-          location="B Building"
-          time="19:00"
-          description="Let's eat some pizza!!!"
-          eventKey="1"
-        />
-        <PublicEvent
-          name="CodeCamp 2021💻"
-          location="Mithat Coruh"
-          time="10:00"
-          description="Littest code party of 2021"
-          eventKey="2"
+          name={data[index].getName()}
+          location={data[index].getLocation()}
+          time={data[index].getTimeRequested()}
+          description={data[index].getDescription()}
+          eventKey={data[index].getIsOpen()}
         />
       </div>
-    </div>
+        );
+      })}
+    
+    
+    
+    
+    
+    
+    
+    </>
   );
+
 }
-
-
-
 export default App;
--- src/components/PublicEvent.js
+++ src/components/PublicEvent.js
@@ -1,8 +1,19 @@
 import { Accordion } from "react-bootstrap";
 
 function PublicEvent(props) {
-  console.log(props);
+  //console.log(props);
+ /*  let e = props.eventKey;
+  if(e == 2){
+    var a = "Public Event";
+  }
+  else if(e == 1){
+    var a = "Campus Only";
+  }
+  if(e == 2){
+    var a = "Private Event";
+  } */
   return (
+    
     <Accordion>
       <Accordion.Item eventKey={props.eventKey}>
         <Accordion.Header>{props.name}</Accordion.Header>
