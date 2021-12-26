953cba8634668a5374729e03e6b73395ed599d12
Doğukan Kurnaz <dogukanertunga@gmail.com>
2021-12-26 03:38:40 +0300

    publicEvents now just only shows PUBLIC ones

--- src/components/PublicEvent.js
+++ src/components/PublicEvent.js
@@ -2,18 +2,9 @@ import { Accordion } from "react-bootstrap";
 
 function PublicEvent(props) {
   //console.log(props);
- /*  let e = props.eventKey;
+ let e = props.eventKey;
   if(e == 2){
-    var a = "Public Event";
-  }
-  else if(e == 1){
-    var a = "Campus Only";
-  }
-  if(e == 2){
-    var a = "Private Event";
-  } */
   return (
-    
     <Accordion>
       <Accordion.Item eventKey={props.eventKey}>
         <Accordion.Header>{props.name}</Accordion.Header>
@@ -25,6 +16,8 @@ function PublicEvent(props) {
       </Accordion.Item>
     </Accordion>
   );
+  }
+  return ""
 }
 
 export default PublicEvent;
